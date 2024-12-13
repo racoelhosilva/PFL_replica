e2ab1421bd0c650d7b06f47e40d3f38b9d5b3279
Process-ing <rbruno.p.o@gmail.com>
2024-12-13 23:25:31 +0000

    Create base structure

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # feup-pfl2
+
+## Instructions to run
+
+```sh
+sicstus -l src/game.pl
+```
---
+++ src/game.pl
@@ -0,0 +1,22 @@
+play.
+
+% initial_state(+GameConfig, -GameState)
+initial_state(_GameConfig, _GameState) :- throw('Not implemented').
+
+% display_game(+GameState)
+display_game(_GameState) :- throw('Not implemented').
+
+% move(+GameState, +Move, -NewGameState)
+move(_GameState, _Move, _NewGameState) :- throw('Not implemented').
+
+% valid_moves(+GameState, -ListOfMoves)
+valid_moves(_GameState, _ListOfMoves) :- throw('Not implemented').
+
+% game_over(+GameState, -Winner)
+game_over(_GameState, _Winner) :- throw('Not implemented').
+
+% value(+GameState, +Player, -Value)
+value(_GameState, _Player) :- throw('Not implemented').
+
+% choose_move(+GameState, +Level, -Move)
+choose_move(_GameState, _Level, _Move) :- throw('Not implemented').
