d4d660c1bd472916da53cb240ac51beaae88543a
racoelhosilva <racoelhosilva@gmail.com>
2025-01-03 13:47:45 +0000

    feat: refactor input_position

--- src/input.pl
+++ src/input.pl
@@ -47,48 +47,57 @@ input_string_aux(String, Accumulator) :-
 input_position(Position, Size):-
     input_position_aux(Position, 0, 0, Size).
 
-% input_position_aux(-Position, +Row, +Col, +Size)
+% input_position_aux(-Position, +Col, +Row, +Size)
 % Auxiliar function to read a position from input
 % Stops at line feed and skips unwanted characters
-input_position_aux(Position, Row, Col, Size):-
-    col_or_size_condition(Col, Size),
+input_position_aux(Position, Col, Row, Size) :-
+    peek_code(10), !, skip_line. 
+input_position_aux(Position, Col, Row, Size) :-
+    can_read_col(Col, Size),
+    read_col(Col, Size, NewCol).
+input_position_aux(Position, Col, Row, Size) :-
+    can_read_row(Row, Size),
+    read_row(Row, Size, NewRow).
+input_position_aux(Position, Col, Row, Size) :-
+    get_code(_),
+    input_position_aux(Position, Row, Col, Size).
+
+% can_read_col(+Col, +Size)
+% Checks if it should read column inputs
+can_read_col(0, _Size).
+can_read_col(_Col, Size) :- Size > 26.
+
+% read_col(+Col, +Size, -NewCol)
+% Reads a column from the input
+read_col(Col, Size, NewCol) :-
     peek_code(Code),
     UpperBoundCalc is 65 + Size - 1,
     UpperBound is min(UpperBoundCalc, 90),
     between(65, UpperBound, Code), !,
     get_code(_),
     NewCol is Col * 26 + (Code - 64),
-    input_position_aux(Position, Row, NewCol, Size).
-input_position_aux(Position, Row, Col, Size):-
-    col_or_size_condition(Col, Size),
+    input_position_aux(Position, NewCol, Row, Size).
+read_col(Col, Size, NewCol) :-
     peek_code(Code),
     UpperBoundCalc is 97 + Size - 1,
     UpperBound is min(UpperBoundCalc, 122),
     between(97, UpperBound, Code), !,
     get_code(_),
     NewCol is Col * 26 + (Code - 96),
-    input_position_aux(Position, Row, NewCol, Size).
-input_position_aux(Position, Row, Col, Size):-
-    row_or_size_condition(Row, Size),
+    input_position_aux(Position, NewCol, Row, Size).
+
+% can_read_row(+Row, +Size)
+% Checks if it should read row inputs
+can_read_row(0, _Size).
+can_read_row(_Row, Size) :- Size > 10.
+
+% read_row(+Row, +Size, -NewRow)
+% Reads a row from the input
+read_row(Row, Size, NewRow) :-
     peek_code(Code),
     UpperBoundCalc is 49 + Size - 1,
     UpperBound is min(UpperBoundCalc, 57),
-    between(46, UpperBound, Code), !,
+    between(48, UpperBound, Code), !,
     get_code(_),
     NewRow is 10 * Row + (Code - 48),
-    input_position_aux(Position, NewRow, Col, Size).
-input_position_aux(Col-Row, Row, Col, _Size):-
-    peek_code(10), !, skip_line.
-input_position_aux(Position, Row, Col, Size):-
-    get_code(_),
-    input_position_aux(Position, Row, Col, Size).
-
-% col_or_size_condition(+Col, +Size)
-% Check if should read more column input
-col_or_size_condition(0, Size).
-col_or_size_condition(_Col, Size) :- Size > 26.
-
-% row_or_size_condition(+Row, +Size)
-% Check if should read more row input
-row_or_size_condition(0, Size).
-row_or_size_condition(_Row, Size) :- Size > 10.
+    input_position_aux(Position, Col, NewRow, Size).
