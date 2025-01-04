7835286dd0d4681bac7f982e9999f92bef2fa186
Process-ing <rbruno.p.o@gmail.com>
2025-01-04 14:04:24 +0000

    Place missing full stops (né Rodrigo)

--- src/config.pl
+++ src/config.pl
@@ -1,9 +1,9 @@
 % config_name(+GameConfig, +Player, -Name)
-% Get the name of the given player in the given game configuration
+% Get the name of the given player in the given game configuration.
 config_name(game_config(_, player_info(Name, _), _), white, Name).
 config_name(game_config(_, _, player_info(Name, _)), black, Name).
 
 % config_difficulty(+GameConfig, +Player, -Difficulty)
-% Get the difficulty of the given player in the given game configuration
+% Get the difficulty of the given player in the given game configuration.
 config_difficulty(game_config(_, player_info(_, Difficulty), _), white, Difficulty).
 config_difficulty(game_config(_, _, player_info(_, Difficulty)), black, Difficulty).
--- src/input.pl
+++ src/input.pl
@@ -2,13 +2,13 @@
 :- use_module(library(lists)).
 
 % read_number(-Number)
-% Reads a number from the input
+% Reads a number from the input.
 read_number(Number):-
     read_number_aux(Number, 0).
 
 % read_number_aux(-Number, +Accumulator)
-% Auxiliar function to read a number from input
-% Stops at line feed and skips unwanted characters
+% Auxiliar predicate to read a number from input.
+% Stops at line feed and skips unwanted characters.
 read_number_aux(Number, Number) :-
     peek_code(10), !, skip_line.
 read_number_aux(Number, Accumulator):- 
@@ -22,13 +22,13 @@ read_number_aux(Number, Accumulator) :-
     read_number_aux(Number, Accumulator).
 
 % read_string(-String)
-% Reads a string from the input
+% Reads a string from the input.
 read_string(String):-
     read_string_aux(String, []).
 
 % read_string_aux(-String, +Accumulator)
-% Auxiliar function to read a string from input
-% Stops at line feed and skips unwanted characters
+% Auxiliar predicate to read a string from input.
+% Stops at line feed and skips unwanted characters.
 read_string_aux(String, Accumulator) :-
     peek_code(10), !, skip_line,
     atom_codes(String, Accumulator).
@@ -43,18 +43,18 @@ read_string_aux(String, Accumulator) :-
     read_string_aux(String, Accumulator).
 
 % read_position(-Position, +Size)
-% Reads a position from the input
-% Letters are associated with columns and numbers are associated with rows
-% If the row or column only needs one character, the first valid character is considered
-% If they need multiple characters, all of them are considered in concatenation
-% Letters can be uppercase or lowercase and any order of rows/columns is considered equivalent
-% Unwanted characters are skipped until the end of the line
+% Reads a position from the input.
+% Letters are associated with columns and numbers are associated with rows.
+% If the row or column only needs one character, the first valid character is considered.
+% If they need multiple characters, all of them are considered in concatenation.
+% Letters can be uppercase or lowercase and any order of rows/columns is considered equivalent.
+% Unwanted characters are skipped until the end of the line.
 read_position(Position, Size):-
     read_position_aux(Position, 0, 0, Size).
 
 % read_position_aux(-Position, +Col, +Row, +Size)
-% Auxiliar function to read a position from input
-% Stops at line feed and skips unwanted characters
+% Auxiliar predicate to read a position from input.
+% Stops at line feed and skips unwanted characters.
 read_position_aux(Col-Row, Col, Row, _Size) :-
     peek_code(10), !, skip_line. 
 read_position_aux(Position, Col, Row, Size) :-
@@ -70,12 +70,12 @@ read_position_aux(Position, Col, Row, Size) :-
     read_position_aux(Position, Col, Row, Size).
 
 % can_read_col(+Col, +Size)
-% Checks if it should read column inputs
+% Checks if it should read column inputs.
 can_read_col(0, _Size).
 can_read_col(_Col, Size) :- Size > 26.
 
 % read_col(+Col, +Size, -NewCol)
-% Reads a column from the input
+% Reads a column from the input.
 read_col(Col, Size, NewCol) :-
     peek_code(Code),
     UpperBoundCalc is 65 + Size - 1,
@@ -92,12 +92,12 @@ read_col(Col, Size, NewCol) :-
     NewCol is Col * 26 + (Code - 96).
 
 % can_read_row(+Row, +Size)
-% Checks if it should read row inputs
+% Checks if it should read row inputs.
 can_read_row(0, _Size).
 can_read_row(_Row, Size) :- Size > 10.
 
 % read_row(+Row, +Size, -NewRow)
-% Reads a row from the input
+% Reads a row from the input.
 read_row(Row, Size, NewRow) :-
     peek_code(Code),
     UpperBoundCalc is 49 + Size - 1,
