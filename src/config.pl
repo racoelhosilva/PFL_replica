b8b4e5cef2b350b2861c7b866cd6d57ee7a35496
racoelhosilva <racoelhosilva@gmail.com>
2025-01-04 18:59:33 +0000

    docs: improve demonstration code

--- src/config.pl
+++ src/config.pl
@@ -7,3 +7,10 @@ config_name(game_config(_, _, player_info(Name, _)), black, Name).
 % Returns the difficulty of the given player in the given game configuration.
 config_difficulty(game_config(_, player_info(_, Difficulty), _), white, Difficulty).
 config_difficulty(game_config(_, _, player_info(_, Difficulty)), black, Difficulty).
+
+
+/* FOR THE DEMONSTRATION */
+
+% demo_config(-GameConfig)
+% Returns an example game configuration for demonstration purposes.
+demo_config(game_config(1, player_info('Bruno', 0), player_info('Rodrigo', 0))).
