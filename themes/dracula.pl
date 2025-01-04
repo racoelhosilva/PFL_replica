a9e310e1067a486acc410ca92428077603366bf4
racoelhosilva <racoelhosilva@gmail.com>
2025-01-04 19:17:34 +0000

    feat: added interface themes

---
+++ themes/dracula.pl
@@ -0,0 +1,111 @@
+/* Text Configurations */
+
+% background
+% Background color of the application.
+background(color(40, 42, 54)).
+
+% foreground
+% Color of the logo on top of the interface.
+logo_color(color(248, 248, 242)).
+
+% menu_header_color
+% Color of the menu header text.
+menu_header_color(color(68, 71, 90)).
+
+% menu_options_color
+% Color of the menu options text.
+menu_options_color(color(98, 114, 164)).
+
+% prompt_color
+% Color of the prompt text in the interface.
+prompt_color(color(68, 71, 90)).
+
+% input_color
+% Color of the input text in the interface.
+input_color(color(248, 248, 242)).
+
+% error_color
+% Color of the error text in the interface.
+error_color(color(255, 85, 85)).
+
+% value_color
+% Color of the board evaluation in the interface.
+value_color(color(189, 147, 249)).
+
+/* Board Configurations */
+
+% board_white
+% Color of "white" tiles on the board.
+board_white(color(248, 248, 242)).
+
+% board_black
+% Color of "black" tiles on the board.
+board_black(color(40, 42, 54)).
+
+% piece_white
+% Color of the "white" pieces on the board.
+piece_white(color(255, 121, 198)).
+
+% white_pawn_symbol
+% Symbol of the "white" pawn.
+white_pawn_symbol('W').
+
+% white_king_symbol
+% Symbol of the "white" king.
+white_king_symbol('*W*').
+
+% piece_black
+% Color of the "black" pieces on the board.
+piece_black(color(255, 184, 108)).
+
+% black_pawn_symbol
+% Symbol of the "black" pawn.
+black_pawn_symbol('B').
+
+% black_king_symbol
+% Symbol of the "black" king.
+black_king_symbol('*B*').
+
+% board_border
+% Color of the border of the board.
+border_background(color(68, 71, 90)).
+
+% border_text
+% Color of the text on the border of the board.
+border_text(color(248, 248, 242)).
+
+% tile_width
+% Specifies the tile width.
+% Changing this might slightly break the interface.
+% It is recommended to keep the width as an odd number.
+% The default value is 7.
+tile_width(7).
+
+% tile_height
+% Specifies the tile height.
+% Changing this might slightly break the interface.
+% It is recommended to keep the height as an odd number.
+% The default value is 3.
+tile_height(3).
+
+/* Notation Configurations */
+
+% notation_color
+% Color of the notation index.
+notation_color(color(98, 114, 164)).
+
+% vertcal_color
+% Color of the vertical moves.
+vertical_color(color(139, 233, 253)).
+
+% horizontal_color
+% Color of the horizontal moves.
+horizontal_color(color(80, 250, 123)).
+
+% diagonal_color
+% Color of the diagonal moves.
+diagonal_color(color(189, 147, 249)).
+
+% transform_color
+% Color of the transformation moves.
+transform_color(color(241, 250, 140)).
